e11227b30c44d22b60854f30109f7f599c200df5
Vasil <vasilliring@gmail.com>
2023-01-14 13:00:31 +0200

    Add fields

--- ForWhileLoops/Program.cs
+++ ForWhileLoops/Program.cs
@@ -1,6 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-Console.WriteLine("Bye");
-//qweqweqwewqewqqwew
+﻿// For && While Loops Telerik
