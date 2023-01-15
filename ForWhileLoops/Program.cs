9401527696939380a024ac77039d6da07efa414a
Vasil <vasilliring@gmail.com>
2023-01-15 18:06:06 +0200

    changes

--- ForWhileLoops/Program.cs
+++ ForWhileLoops/Program.cs
@@ -33,6 +33,7 @@
 //int number = int.Parse(Console.ReadLine());
 
 //for (int i = 1; i <= number; i++)
+
 //{
 
 //    if (i % 3 == 0 || i % 7 == 0)
@@ -48,13 +49,12 @@
 //string number =Console.ReadLine();
 
 //for (int i = number.Length -1; i >= 0; i--)
+
 //{
+
 //    Console.Write(number[i]);
+
 //}
-
-
-
-
 
 
 
