8e93dbb0e32ac1aa19d24ab09f7553e521fea4f4
Vasil <vasilliring@gmail.com>
2023-01-15 18:04:13 +0200

    New tasks

--- ForWhileLoops/Program.cs
+++ ForWhileLoops/Program.cs
@@ -1,1 +1,58 @@
 ﻿// For && While Loops Telerik
+
+// Task 1
+
+//Not Divisible Number
+
+//Description
+//Write a program that reads from the console a positive integer N and prints all the numbers from 1 to N not divisible by 3 or 7
+
+//Input
+//Will always consists of one valid integer number - the number N.
+
+//Output
+//Should always consists of the numbers from 1 to N, which are not divisible by 3 or 7, separated by a whitespace.
+
+//Constraints
+//1 < N < 1500
+
+//Sample tests
+//Input    Output
+//10    1 2 4 5 8 10
+//3    1 2
+
+//Task 2
+
+//Write a method that reverses the digits of a given decimal number. - hint: use string instead of decimal to iterate
+// Input 70  Output 07
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+// Not Divisible Number
+
+//int number = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i <= number; i++)
+//{
+
+//    if (i % 3 == 0 || i % 7 == 0)
+
+//        continue;
+
+//        Console.Write($"{i} ");
+
+//}
+
+// Reversed Number
+
+//string number =Console.ReadLine();
+
+//for (int i = number.Length -1; i >= 0; i--)
+//{
+//    Console.Write(number[i]);
+//}
+
+
+
+
+
