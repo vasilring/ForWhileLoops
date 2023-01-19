a72de139f1d52876e23827f1a4071e1de4bb1dd0
Vasil <vasilliring@gmail.com>
2023-01-19 16:27:30 +0200

    new

--- ForWhileLoops/Program.cs
+++ ForWhileLoops/Program.cs
@@ -589,3 +589,56 @@
 //{
 //	Console.WriteLine($"{vowel} {cons}");
 //}
+
+//---------------------------------------------------------------------
+
+//New Task
+//Word or number
+//Description
+//Write a program that reads from the console and checks if the input is a number or a word.
+
+//If the input is a word, it should be reversed.
+//If it is a number add 1 to it.
+//Input
+//The input is text on a single line (without intervals)
+//If the input is a word it won't contain any digits!
+//Output
+//Print on a single line the reversed text or increased by 1 number
+//Sample tests
+//Input
+//good
+//Output
+//doog
+//Input
+//32
+//Output
+//33
+
+
+//string message = Console.ReadLine();
+//string msg = string.Empty;
+
+
+//if (char.IsLetter(message[0]))
+//{
+//	for (int i = message.Length - 1; i >= 0; i--)
+//	{
+//		if (char.IsLetter(message[i]))
+//		{
+//			msg += message[i];
+//		}
+
+
+//	}
+//	Console.WriteLine(msg);
+//}
+
+//double n = 0;
+//bool number = double.TryParse(message, out n);
+//if (number)
+//{
+//	n++;
+//	Console.WriteLine($"{n}");
+
+//}
+
